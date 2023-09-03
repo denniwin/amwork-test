b1f5dd91a39c93af1042ea4d7907ad1d762ded94
Den <denis032014@bk.ru>
2023-09-03 22:52:09 +0300

    Мелкие правки по функционалу

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import TodoHeader from "./components/todo-header/todos-header";
 import TodoItem from "./components/todo-item/todos-item";
 import {GetTodos} from "./services/API/todos.api";
@@ -10,19 +10,7 @@ function App() {
   const [todos, setTodos] = useState<Todos[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(2);
   const [fetching, setFetching] = useState<boolean>(false);
-
-  const scrollHandler = (e: Event) => {
-    if (
-      e.target instanceof Document &&
-      e.target.documentElement.scrollHeight -
-        (e.target.documentElement.scrollTop + window.innerHeight) <
-        100
-    ) {
-      setFetching(true);
-    }
-  };
-
-  const debouncedScrollHandler = useDebouncedFunction(scrollHandler, 300);
+  const divRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     GetTodos().then((td) => setTodos(td));
@@ -40,16 +28,32 @@ function App() {
   }, [fetching]);
 
   useEffect(() => {
-    document.addEventListener("scroll", debouncedScrollHandler);
-    return () => {
-      document.removeEventListener("scroll", debouncedScrollHandler);
+    const container = document.querySelector(".todos");
+    container && container.addEventListener("scroll", debouncedScrollHandler);
+      return () => {
+      container && container.removeEventListener("scroll", debouncedScrollHandler);
     };
   }, []);
 
+
+  const scrollHandler = () => {
+    const divElement = divRef.current;
+
+    if (divElement) {
+      if (
+        (divElement as HTMLElement).scrollTop + 300  > window.innerHeight
+      ) {
+        setFetching(true);
+      }
+}
+  };
+
+  const debouncedScrollHandler = useDebouncedFunction(scrollHandler, 300);
+
   return (
     <>
-      <div className="todos">
-        <TodoHeader />
+      <div className="todos" ref={divRef}>
+        <TodoHeader todos={todos}/>
         {todos && todos.map((todo) => <TodoItem todo={todo} key={todo.id} />)}
       </div>
     </>
--- src/components/todo-header/todos-header.tsx
+++ src/components/todo-header/todos-header.tsx
@@ -1,13 +1,14 @@
 import add from "../../assets/add.svg";
-import count from "../../assets/count.svg";
+import { Todos } from "../../services/models/todos";
 
-const TodoHeader = () => {
+const TodoHeader = ({ todos }: { todos: Todos[] }) => {
+  
   return (
     <div className="todos__header">
       <div className="todos__header-title">Today</div>
       <div className="todos__actions">
-        <img className="todos__actions-img" src={add} alt="add-todo" />
-        <img className="todos__actions-img" src={count} alt="count-todo" />
+        <img className="todos__actions-add" src={add} alt="add-todo" />
+        <div className="todos__actions-count"><span>{todos.length}</span></div>
       </div>
     </div>
   );
